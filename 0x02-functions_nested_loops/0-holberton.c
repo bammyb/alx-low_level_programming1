9abe587f601ceff7655751d51c4c192f8ed7238c
bammyb <adelakunbamidele@gmail.com>
2021-06-22 01:47:48 -0700

    this program prints hoberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*preprocessor*/
+
+/*this is the main entry to the program*/
+int main(void)
+{
+/*the code goes here*/
+/*decleration*/
+
+/*the original code*/
+printf("Holberton\n");
+
+return (0);
+
+}
