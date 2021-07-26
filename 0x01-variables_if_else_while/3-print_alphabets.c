283260461e72337594597ccf542c1e540bfab2c1
bammyb <adelakunbamidele@gmail.com>
2021-07-26 06:09:25 -0700

    adding project 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more header goes here*/
+
+/**
+ * main - Entry point
+ * *
+ * prints lower case alphabet
+ *
+ * Return: Always 0(Success)
+ * **/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	for (char a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
