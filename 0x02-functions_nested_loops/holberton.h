96a374f86b5c86bf33c1b8adc00b851c50d88f6c
bammyb <adelakunbamidele@gmail.com>
2021-08-02 07:40:06 -0700

    This is changing holberton .h file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,19 +1,6 @@
 #ifndef holberton
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * character in array to screen
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-#define holberton
+#define holberton.h
 #endif
