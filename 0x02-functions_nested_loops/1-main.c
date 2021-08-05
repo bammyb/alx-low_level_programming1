209ad55effab646c11abf4bcbb5cfa27ce60da6e
bammyb <adelakunbamidele@gmail.com>
2021-08-05 00:16:01 -0700

    adding alphabe and main.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/*more pre processor around*/
+
+/**
+ * main - Print alphabet in lower case
+ *
+ * @void - dose not take any parameter
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
